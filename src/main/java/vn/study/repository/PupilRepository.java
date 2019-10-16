a4657cbb712d25d61827b430046d7bd023991cd7
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 14:29:31 +0700

    add method findByLearnClass()

--- src/main/java/vn/study/repository/PupilRepository.java
+++ src/main/java/vn/study/repository/PupilRepository.java
@@ -1,6 +1,9 @@
 package vn.study.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import vn.study.model.LearnClass;
 import vn.study.model.Pupil;
 
-public interface PupilRepository extends PagingAndSortingRepository<Pupil, Long> {}
+public interface PupilRepository extends PagingAndSortingRepository<Pupil, Long> {
+    Iterable<Pupil> findAllByLearnClass(LearnClass learnClass);
+}
