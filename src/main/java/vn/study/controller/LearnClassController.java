060e32ed950853416a02b6b16482f58603b52345
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 14:51:44 +0700

    controller view class

--- src/main/java/vn/study/controller/LearnClassController.java
+++ src/main/java/vn/study/controller/LearnClassController.java
@@ -2,10 +2,21 @@ package vn.study.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import vn.study.model.LearnClass;
 import vn.study.service.LearnClassService;
 
 @Controller
 public class LearnClassController {
     @Autowired
     private LearnClassService learnClassService;
+    @GetMapping("/class")
+    public ModelAndView listLearnClass(){
+        Iterable<LearnClass> learnClasses = learnClassService.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("/class/list");
+        modelAndView.addObject("class", learnClasses);
+        return modelAndView;
+    }
 }
