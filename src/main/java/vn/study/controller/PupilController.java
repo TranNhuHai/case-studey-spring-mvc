ad28ca54d899194c2461fd9fc012cf5053be2ecf
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 11:48:00 +0700

    Add new pupil

--- src/main/java/vn/study/controller/PupilController.java
+++ src/main/java/vn/study/controller/PupilController.java
@@ -3,6 +3,8 @@ package vn.study.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import vn.study.model.Pupil;
 import vn.study.service.PupilService;
@@ -25,6 +27,14 @@ public class PupilController {
     public ModelAndView showCreateForm() {
         ModelAndView modelAndView = new ModelAndView("/pupil/create");
         modelAndView.addObject("pupil", new Pupil());
+        return modelAndView;
+    }
+    @PostMapping("/create-pupil")
+    public ModelAndView savePupil(@ModelAttribute("pupil") Pupil pupil){
+        pupilService.save(pupil);
+        ModelAndView modelAndView = new ModelAndView("/pupil/create");
+        modelAndView.addObject("pupil", new Pupil());
+        modelAndView.addObject("message", "add new pupil successfully");
         return modelAndView;
     }
 }
