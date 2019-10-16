89ae919d03651268d960e96b83a85331e194d65e
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 15:29:33 +0700

    controller create class

--- src/main/java/vn/study/controller/LearnClassController.java
+++ src/main/java/vn/study/controller/LearnClassController.java
@@ -3,6 +3,8 @@ package vn.study.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import vn.study.model.LearnClass;
 import vn.study.service.LearnClassService;
@@ -17,6 +19,22 @@ public class LearnClassController {
 
         ModelAndView modelAndView = new ModelAndView("/class/list");
         modelAndView.addObject("learnClasses", learnClasses);
+        return modelAndView;
+    }
+
+    @GetMapping("/create-class")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/class/create");
+        modelAndView.addObject("learnClass", new LearnClass());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-class")
+    public ModelAndView savePupil(@ModelAttribute("learnClass") LearnClass pupil) {
+        learnClassService.save(pupil);
+        ModelAndView modelAndView = new ModelAndView("/class/create");
+        modelAndView.addObject("learnClass", new LearnClass());
+        modelAndView.addObject("message", "add new successfully");
         return modelAndView;
     }
 }
