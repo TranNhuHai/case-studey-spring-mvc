8761af75766523238a4a5aa13a6ae82dd13a524e
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 09:10:33 +0700

    edit interface pupilService

--- src/main/java/vn/study/service/PupilService.java
+++ src/main/java/vn/study/service/PupilService.java
@@ -1,4 +1,14 @@
 package vn.study.service;
 
+import vn.study.model.Pupil;
+
 public interface PupilService {
+
+    Iterable<Pupil> findAll();
+
+    Pupil findById(Long id);
+
+    void save(Pupil pupil);
+
+    void remove(Long id);
 }
