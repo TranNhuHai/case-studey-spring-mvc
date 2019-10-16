88ae7eadb2751d1d6213b4ce878f9befc5009ef8
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 09:30:03 +0700

    code controller

--- src/main/java/vn/study/controller/PupilController.java
+++ src/main/java/vn/study/controller/PupilController.java
@@ -1,4 +1,23 @@
 package vn.study.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import vn.study.model.Pupil;
+import vn.study.service.PupilService;
+
+@Controller
 public class PupilController {
+    @Autowired
+    private PupilService pupilService;
+    @GetMapping("/pupils")
+    public ModelAndView listPupils(){
+        Iterable<Pupil> pupils = pupilService.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("/pupil/list");
+        modelAndView.addObject("pupils", pupils);
+        return modelAndView ;
+    }
+
 }
