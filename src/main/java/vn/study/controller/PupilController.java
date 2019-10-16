0712fe899d7352034844278be15dd81718dec490
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 11:53:18 +0700

    Add controller Edit

--- src/main/java/vn/study/controller/PupilController.java
+++ src/main/java/vn/study/controller/PupilController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import vn.study.model.Pupil;
@@ -29,12 +30,36 @@ public class PupilController {
         modelAndView.addObject("pupil", new Pupil());
         return modelAndView;
     }
+
     @PostMapping("/create-pupil")
-    public ModelAndView savePupil(@ModelAttribute("pupil") Pupil pupil){
+    public ModelAndView savePupil(@ModelAttribute("pupil") Pupil pupil) {
         pupilService.save(pupil);
         ModelAndView modelAndView = new ModelAndView("/pupil/create");
         modelAndView.addObject("pupil", new Pupil());
         modelAndView.addObject("message", "add new pupil successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/edit-pupil/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Pupil pupil = pupilService.findById(id);
+        if (pupil != null) {
+            ModelAndView modelAndView = new ModelAndView("/pupil/edit");
+            modelAndView.addObject("pupil", pupil);
+            return modelAndView;
+
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-pupil")
+    public ModelAndView updatePupil(@ModelAttribute("pupil") Pupil pupil) {
+        pupilService.save(pupil);
+        ModelAndView modelAndView = new ModelAndView("/pupil/edit");
+        modelAndView.addObject("pupil", pupil);
+        modelAndView.addObject("message", " Updated successfully");
         return modelAndView;
     }
 }
