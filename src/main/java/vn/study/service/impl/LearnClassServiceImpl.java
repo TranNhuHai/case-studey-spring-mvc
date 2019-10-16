31119ba22cd0ff3e7bc70158434ad9b2eb63f221
TranNhuHai <tranhaiexp@gmail.com>
2019-10-16 14:38:12 +0700

    create class LearnClassServiceImpl()

---
+++ src/main/java/vn/study/service/impl/LearnClassServiceImpl.java
@@ -0,0 +1,32 @@
+package vn.study.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import vn.study.model.LearnClass;
+import vn.study.repository.LearnClassRepository;
+import vn.study.service.LearnClassService;
+
+public class LearnClassServiceImpl implements LearnClassService {
+    @Autowired
+    LearnClassRepository learnClassRepository;
+
+    @Override
+    public Iterable<LearnClass> findAll() {
+        return learnClassRepository.findAll();
+    }
+
+    @Override
+    public LearnClass findById(int id) {
+        return learnClassRepository.findOne(id);
+    }
+
+    @Override
+    public void save(LearnClass learnClass) {
+        learnClassRepository.save(learnClass);
+    }
+
+    @Override
+    public void remove(int id) {
+        learnClassRepository.delete(id);
+
+    }
+}
